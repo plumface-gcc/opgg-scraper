771fe2173a39a7e6cda35bf9fe98f3ec323b3f39
time007gc <200422828@student.georgianc.on.ca>
2020-02-10 14:50:19 -0500

    adding position of scraper
    outputs where it is currently scraping from

--- opgg scraper.py
+++ opgg scraper.py
@@ -7,23 +7,19 @@ names_x2 = list() #storing all data in a list
 myInput = int(input("What ladder rank do you want to start on?: "))
 myInput2 = int(input("How many accounts do you want to parse?: "))
 
-for i in range(myInput, myInput + myInput2, 50):
+for j in range(myInput, myInput + myInput2, 50):
     for i in [myInput]:
-        destinationUrl = opggUrl + str(i) 
+        destinationUrl = opggUrl + str(j) 
         page = requests.get(destinationUrl)
         tree = html.fromstring(page.content) #grabbing page html
         names_x1 = tree.xpath('//a[not(@target) and not(@onclick)]/text()') #getting the xpath of a specific part of the page 
         names_x2.extend(names_x1)
+        print("At position: ", [j])
 
-try:
-    with open("scrape.txt", "w", encoding="cp65001") as f:
-        for item in names_x2:
-            print("\n") 
-            f.write("%s\n" % item) #writing to txt file
-except:
-    print("You must have a text file named scrape.txt for this to pass data into.")
-    time.sleep(5)
-    sys.exit()
+with open("scrape.txt", "w", encoding="cp65001") as f:
+    for item in names_x2:
+        print("\n") 
+        f.write("%s\n" % item) #writing to txt file
         
 print("\n")
 print(len(names_x2), "names parsed.")
