c9a9a1ef1e314bc9c1711e01d96276ddb9f956fb
time007gc <200422828@student.georgianc.on.ca>
2020-02-13 01:51:47 -0500

    adding exception handling

--- combo maker.py
+++ combo maker.py
@@ -3,15 +3,30 @@ import random
 
 fileInput = "scrape.txt"
 
-passwordInput1 = str(input("Password 1 to combo to: "))
-passwordInput2 = str(input("Password 2 to combo to: "))
-passwordInput3 = str(input("Password 3 to combo to: "))
+while True:
+    try:
+        passwordInput1 = str(input("Password 1 to combo to: "))
+        passwordInput2 = str(input("Password 2 to combo to: "))
+        passwordInput3 = str(input("Password 3 to combo to: "))
+        print("\n")
 
-passwordBox = [passwordInput1, passwordInput2, passwordInput3] #putting this into a list for later
+    except ValueError:  # check if entered password is not empty
+        print("Please enter a password.", "\n")
+        time.sleep(2)
+        continue
 
-with open(fileInput) as myFile: #opening our file input
-    lines = myFile.read().splitlines() #read, then split at line breaks, giving us a list of lines to work with
+    else:
+        break
+
+passwordBox = [passwordInput1, passwordInput2, passwordInput3]  # putting this into a list for later
+
+try:
+    with open(fileInput) as myFile:  # opening our file input
+        lines = myFile.read().splitlines()  # read, then split at line breaks, giving us a list of lines to work with
+
+except UnicodeDecodeError:  # catching if a string contains unicode that will not work with our chosen character encoding
+    names_x2.remove() #if so, remove it from our list
 
 with open(fileInput, 'w') as myFile:
-    for line in lines: #looping through our file
-        print(line + ":" + random.choice(passwordBox), file=myFile) #per username, randomly assign a password given through a list
+    for line in lines:  # looping through our file
+        print(line + ":" + random.choice(passwordBox), file=myFile)  # per username, randomly assign a password given through a list
--- opggscraper.py
+++ opggscraper.py
@@ -2,25 +2,36 @@ from lxml import html
 import requests, sys, locale, os, codecs, time
 
 opggUrl = "https://na.op.gg/ranking/ajax2/ladders/start=" #where to start from
-names_x2 = list() #storing all data here
-myInput = int(input("What ladder rank do you want to start on?: "))
-myInput2 = int(input("How many accounts do you want to parse?: "))
+names_x2 = list()  # storing all data here
+
+while True:
+    try:
+        myInput = int(input("What ladder rank do you want to start on?: "))
+        myInput2 = int(input("How many accounts do you want to parse?: "))
+
+    except ValueError:
+        print("Please enter an integer.", "\n")
+        time.sleep(2)
+        continue
+
+    else:
+        break
 
 for j in range(myInput, myInput + myInput2, 50):
     for i in [myInput]:
         destinationUrl = opggUrl + str(i)
         page = requests.get(destinationUrl)
-        tree = html.fromstring(page.content) #grabbing page html
-        names_x1 = tree.xpath('//a[not(@target) and not(@onclick)]/text()') #grabbing specific html content, this being the username
-        names_x2.extend(names_x1) #adding on to our list
+        tree = html.fromstring(page.content)  # grabbing page html
+        names_x1 = tree.xpath('//a[not(@target) and not(@onclick)]/text()')  # grabbing specific html content, this being the username
+        names_x2.extend(names_x1)  # adding on to our list
         print("At position: ", [j], "\n")
 
 with open("scrape.txt", "w", encoding="cp65001") as f:
     for item in names_x2:
-        print("\n") 
-        f.write("%s\n" % item) #writing to txt file
+        f.write("%s\n" % item)  # writing to txt file
         
 print("\n")
 print(len(names_x2), "names parsed.")
 print("\n")
-print("All data passed to text file successfully.")
+print("All data passed to text file successfully.", "\n\n\n")
+time.sleep(2)
